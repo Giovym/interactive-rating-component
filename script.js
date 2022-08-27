edea9037e33beeb23fdfe663fa91822c0dbce892
Giovanni <98093812+Giovym@users.noreply.github.com>
2022-08-27 18:04:01 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// You selected 4 out of 5
+const ratingContainer = document.getElementById("rating");
+const thankYouContainer = document.getElementById("thank-you");
+const scores = document.querySelectorAll(".score");
+const textThankYou = document.querySelector(".p-thank-you");
+const button = document.querySelector(".btn");
+
+scores.forEach((s) =>
+  s.addEventListener("click", function (e) {
+    const clicked = e.target;
+    scores.forEach((sc) => sc.classList.remove("score--active"));
+    clicked.classList.add("score--active");
+    if (!clicked) return; // Guarde clause
+    button.addEventListener("click", function () {
+      ratingContainer.classList.add("display-none");
+      thankYouContainer.classList.remove("display-none");
+      textThankYou.textContent = `You selected ${clicked.dataset.score} out of 5`;
+    });
+  })
+);
